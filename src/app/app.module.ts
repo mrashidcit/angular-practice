7570e73f0212c817abf69f39a345581e1cdd2985
mrashidcit <mrashidcit@gmail.com>
2017-05-22 14:37:50 +0500

    Milestone 3: Heroes Features  (Completed)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,31 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {AppComponent} from "./app.component";
-import {RouterModule, Routes} from '@angular/router';
+
 
 
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
-import { HeroListComponent } from './hero-list/hero-list.component';
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {AppRoutingModule} from "./app-routing.module";
+import {HeroesModule} from "./heroes/heroes.module";
 
 
-const appRoutes: Routes = [
-    { path: 'crisis-center', component: CrisisListComponent},
-    { path: 'heroes', component: HeroListComponent},
 
-    { path: '', redirectTo: '/heroes' , pathMatch: 'full' },
-    { path: '**', component: PageNotFoundComponent},
-
-
-]
 
 
 @NgModule({
   declarations: [
       AppComponent,
       CrisisListComponent,
-      HeroListComponent,
-      PageNotFoundComponent
+      PageNotFoundComponent,
+
 
 
 
@@ -35,7 +28,10 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    HeroesModule,
+    AppRoutingModule,
+
+
 
 
 
